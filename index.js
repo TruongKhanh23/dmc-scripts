68a1576c9b8f0434e61edb80c8e31f60a823d824
Trương Khánh <truongnguyenkhanh230800@gmail.com>
2025-05-13 20:24:12 +0700

    refactor

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-//import data from "./data/products_hethongchung.js";
+import fs from "fs";
 import products from "./data/products_master.js";
 import generateData from "./javascript/generate-data/generateProductData.js";
-import fs from "fs";
 import generateSQLInsertProducts from "./javascript/generate-sql/generateSQLScript_InsertProducts.js";
 
 const data = generateData(products);
-
-// Gọi hàm để generate SQL
 const sql = generateSQLInsertProducts(data);
 
 fs.writeFileSync("result.txt", sql, "utf8");
-
 //fs.writeFileSync("result.txt", JSON.stringify(data, null, 2), "utf8");
 
 console.log("Data đã được ghi vào file result.txt");
