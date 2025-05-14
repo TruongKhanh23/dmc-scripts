46e74acaba4f5babbe31b5b781287033e063ad1f
Trương Khánh <truongnguyenkhanh230800@gmail.com>
2025-05-14 20:03:17 +0700

    insert_sale_channels add code

--- sql-scripts/insert/insert_sale_channels.sql
+++ sql-scripts/insert/insert_sale_channels.sql
@@ -3,43 +3,43 @@ BEGIN
     -- Kiểm tra xem kênh 'Tại quán' đã tồn tại chưa
     IF NOT EXISTS (SELECT 1 FROM public.sales_channels WHERE name = 'Tại quán') THEN
         INSERT INTO public.sales_channels(
-            id, created_by, updated_by, created_at, updated_at, deleted, name, description
-        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'Tại quán', 'Kênh bán hàng tại quán');
+            id, created_by, updated_by, created_at, updated_at, deleted, name, description, code
+        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'Tại quán', 'Kênh bán hàng tại quán', 'at_branch');
     END IF;
 
     -- Kiểm tra xem kênh 'Grab' đã tồn tại chưa
     IF NOT EXISTS (SELECT 1 FROM public.sales_channels WHERE name = 'Grab') THEN
         INSERT INTO public.sales_channels(
-            id, created_by, updated_by, created_at, updated_at, deleted, name, description
-        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'Grab', 'Kênh bán hàng qua Grab');
+            id, created_by, updated_by, created_at, updated_at, deleted, name, description, code
+        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'Grab', 'Kênh bán hàng qua Grab', 'grab');
     END IF;
 
     -- Kiểm tra xem kênh 'Shopee' đã tồn tại chưa
     IF NOT EXISTS (SELECT 1 FROM public.sales_channels WHERE name = 'Shopee') THEN
         INSERT INTO public.sales_channels(
-            id, created_by, updated_by, created_at, updated_at, deleted, name, description
-        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'Shopee', 'Kênh bán hàng trên Shopee');
+            id, created_by, updated_by, created_at, updated_at, deleted, name, description, code
+        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'Shopee', 'Kênh bán hàng trên Shopee', 'shopee');
     END IF;
 
     -- Kiểm tra xem kênh 'Be' đã tồn tại chưa
     IF NOT EXISTS (SELECT 1 FROM public.sales_channels WHERE name = 'Be') THEN
         INSERT INTO public.sales_channels(
-            id, created_by, updated_by, created_at, updated_at, deleted, name, description
-        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'Be', 'Kênh bán hàng qua Be');
+            id, created_by, updated_by, created_at, updated_at, deleted, name, description, code
+        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'Be', 'Kênh bán hàng qua Be', 'be');
     END IF;
 
     -- Kiểm tra xem kênh 'VILL' đã tồn tại chưa
     IF NOT EXISTS (SELECT 1 FROM public.sales_channels WHERE name = 'VILL') THEN
         INSERT INTO public.sales_channels(
-            id, created_by, updated_by, created_at, updated_at, deleted, name, description
-        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'VILL', 'Kênh bán hàng VILL');
+            id, created_by, updated_by, created_at, updated_at, deleted, name, description, code
+        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'VILL', 'Kênh bán hàng VILL', 'vill');
     END IF;
 
     -- Kiểm tra xem kênh 'RIO' đã tồn tại chưa
     IF NOT EXISTS (SELECT 1 FROM public.sales_channels WHERE name = 'RIO') THEN
         INSERT INTO public.sales_channels(
-            id, created_by, updated_by, created_at, updated_at, deleted, name, description
-        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'RIO', 'Kênh bán hàng RIO');
+            id, created_by, updated_by, created_at, updated_at, deleted, name, description, code
+        ) VALUES (substr(md5(random()::text), 1, 24), 'admin', 'admin', NOW(), NOW(), FALSE, 'RIO', 'Kênh bán hàng RIO', 'rio');
     END IF;
 
 END $$;
