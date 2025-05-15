83c1d6b842bdbb214ae23b9b16e8dccdc9231800
truongn.khanh@.dai-ichi-life.com.vn <truongn.khanh@.dai-ichi-life.com.vn>
2025-05-15 11:04:46 +0700

    update fillInBranchName

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import fs from "fs";
 import products from "./data/products_master.js";
 import generateData from "./javascript/generate-data/generateProductData.js";
 import generateSQLInsertProducts from "./javascript/generate-sql/generateSQLScript_InsertProducts.js";
+import { fillInBranchName } from "./javascript/generate-data/fillInBranchName.js";
 
-const data = generateData(products);
-const sql = generateSQLInsertProducts(data);
+// ---------------- Generate SQL Script - Insert Products ---------------- 
+// const data = generateData(products);
+// const sql = generateSQLInsertProducts(data);
+// fs.writeFileSync("result.txt", sql, "utf8");
 
-fs.writeFileSync("result.txt", sql, "utf8");
-//fs.writeFileSync("result.txt", JSON.stringify(data, null, 2), "utf8");
+// ---------------- Generate Product Data Input for Generate Script Insert Products ---------------- 
+// const data = generateData(products);
+// fs.writeFileSync("result.txt", JSON.stringify(data, null, 2), "utf8");
+
+// ---------------- Fill In Branch Name ---------------- 
+const data = fillInBranchName();
+fs.writeFileSync("result.txt", JSON.stringify(data, null, 2), "utf8");
 
 console.log("Data đã được ghi vào file result.txt");
