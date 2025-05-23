0ec4a7a8d95962c35e0c18e8e102d571154c1d99
truongn.khanh@.dai-ichi-life.com.vn <truongn.khanh@.dai-ichi-life.com.vn>
2025-05-23 09:10:54 +0700

    commonUnít

---
+++ data/commonUnits.js
@@ -0,0 +1,9 @@
+const commonUnits = [
+  { name: "milligram", symbol: "mg" },
+  { name: "gram", symbol: "g" },
+  { name: "kilogram", symbol: "kg" },
+  { name: "milliliter", symbol: "ml" },
+  { name: "liter", symbol: "l" },
+];
+
+export default commonUnits;
